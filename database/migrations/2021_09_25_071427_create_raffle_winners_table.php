b13ef5baf4233798307b7ce9bf0271cf3ce7636f
Tony ID <tony.inuma@gmail.com>
2021-09-25 01:15:37 -0500

    Add: Raffles Winners table

---
+++ database/migrations/2021_09_25_071427_create_raffle_winners_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRaffleWinnersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('raffle_winners', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('raffle_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('raffle_winners');
+    }
+}
