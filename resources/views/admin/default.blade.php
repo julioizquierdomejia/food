3221cd230f9ce7e894c90269dc4dae4f7e146095
Tony ID <tony.inuma@gmail.com>
2021-09-25 22:51:05 -0500

    Add: cdn of repeater js library

--- resources/views/admin/default.blade.php
+++ resources/views/admin/default.blade.php
@@ -45,6 +45,7 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.repeater/1.2.1/jquery.repeater.js"></script>
 
 <!-- End of global js content-->
 
