512bfcdc9a9150e7db111ff3399a9a0c7a36a70c
Tony ID <tony.inuma@gmail.com>
2021-09-27 20:55:39 -0500

    Fix: form edit raffle fields

--- resources/views/admin/raffles/form.blade.php
+++ resources/views/admin/raffles/form.blade.php
@@ -8,18 +8,12 @@
                         <select class="form-control" name="item_id" id="item_id" required>
                             <option value="">-- seleccionar --</option>
                             @foreach($items as $item)
-                                <option value="{{ $item->id }}"
+                                <option value="{{ $item->id }}" price="{{ $item->price }}"
                                         @if($item->id == $raffle->item_id) selected @endif>{{ $item->name }}</option>
                             @endforeach
                         </select>
                     </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="max_tickets_number">Maximo de Tickets (opcional)</label>
-                        <input type="number" name="max_tickets_number" id="max_tickets_number"
-                               class="form-control" value="{{ $raffle->max_tickets_number }}">
-                    </div>
+                    {!! Form::myInput('number', 'raffle_goal_amount', 'Monto Objetivo:', ['required']) !!}
                     <div class="form-group">
                         <label for="start_date">Fecha Inicio</label>
                         <input type="date" name="start_date" id="start_date"
@@ -31,7 +25,31 @@
                                class="form-control" value="{{ $raffle->end_date->format('Y-m-d') }}">
                     </div>
                 </div>
+                <div class="col-md-6">
+                </div>
             </div>
         </div>
     </div>
 </div>
+
+@push('js')
+    <script>
+        $('#item_id').change(function () {
+            const price = $('option:selected', this).attr('price');
+            $('#raffle_goal_amount').val(price);
+        });
+
+        $(document).ready(function () {
+
+            $('.repeater').repeater({
+                hide: function (deleteElement) {
+                    if (confirm('¿Eliminar Elemento?')) {
+                        $(this).slideUp(deleteElement);
+                    }
+                },
+                isFirstItemUndeletable: true
+            })
+
+        });
+    </script>
+@endpush
