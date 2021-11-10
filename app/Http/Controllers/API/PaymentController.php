50b062738566b187b8977536d31c6ebe2cd0fcf4
Nicolas <nicojiart@gmail.com>
2021-11-10 14:39:13 -0500

    Payment

--- app/Http/Controllers/API/PaymentController.php
+++ app/Http/Controllers/API/PaymentController.php
@@ -17,114 +17,125 @@ class PaymentController extends Controller
 {
     use ApiResponse;
 
-    public function paymentTest()
-    {
-        $url = 'https://api.micuentaweb.pe/api-payment/V4/Charge/SDKTest';
-        $account = "72672204:Larifa@@2021";
-        $b64account = base64_encode($account);
-        $headers = ['Authorization' => 'Basic ' . $b64account,
-        'Content-Type'=> 'application/json'];
-        $body = [
-            'value'=> 'my testing value'
-        ];
 
-        $response = Http::withHeaders($headers)->post($url, $body);
-
-
-        return $this->successResponse([
-            'status' => 200,
-            'message' => 'Mail was sent to reset password',
-            'data' =>$response->status()
-        ]);
-    }
     public function paymentCreate(Request $requet)
     {
         try {
-        $data = request()->json()->all();
+            $data = request()->json()->all();
 
 
-        $car = $data['car'];
-        $amount = $data['amount'];
-        $iduser = $data['iduser'];
-        $pan = $data['pan'];
-        $expiryMonth = $data['expiryMonth'];
-        $expiryYear = $data['expiryYear'];
-        $securityCode = $data['securityCode'];
+            $car = $data['car'];
+            $amount = $data['amount'];
+            $iduser = $data['iduser'];
 
-        $user = User::where('id',$iduser)->get()->first();
-        if ($user == null) {
-            return $this->errorResponse('No se encontro el usuario'.' '.$user, 400);
-        }
-        foreach ($car as $key) {
 
-            $raffle = Raffle::where('id',$key['raffle_id'])->get()->first();
-            if ($raffle == null) {
-                return $this->errorResponse('No se encontro la rifa'.' '.$key['raffle_id'], 400);
+            $user = User::where('id', $iduser)->get()->first();
+            if ($user == null) {
+                return $this->errorResponse('No se encontro el usuario' . ' ' . $user, 400);
             }
-        }
+            foreach ($car as $key) {
 
-        $price = $data['price']* 100;
-
-        $order_id = Str::uuid();
-        $username = "44623003";
-        $password = "prodpassword_6LAbudTQG0n381ZefWtuya7W4K0TM7W6TyblWeNHBedBz";
-        $url = 'https://api.micuentaweb.pe/api-payment/V4/Charge/CreatePayment';
-        /* $account = "44623003:testpassword_Rtn87ByTJlAHVXQZ3e3oSaDb8WX2kLzZ2UtSABKyJdSsC";
-        $b64account = base64_encode($account); */
-        $body = array(
-            "amount"=> $price,
-            "currency"=> "PEN",
-            "orderId" => $order_id,
-            "customer"=> array(
-                "email"=> $user->email
-            )
-        );
-
-
-
-        /* $response = Http::withHeaders($headers)->post($url, $body); */
-        $RESPONSE = json_encode($body);
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_USERPWD, $username . ":" . $password);
-        //curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $RESPONSE);
-
-        $response = curl_exec($ch);
-
-        curl_close($ch);
-
-        $res = json_decode($response);
-
-        foreach ($car as $key) {
-
-            $sell = new UserTicket();
-            $sell->user_id = $iduser;
-            $sell->raffles_id = $key['raffle_id'];
-            $sell->quantity = $key['amount'];
-            if ($res->status == "ERROR") {
-                $sell->status = 'failed';
-                $sell->oreder_id = $order_id;
-                $sell->save();
-            }else{
-                $sell->status = 'confirmed';
-                $sell->oreder_id = $order_id;
-                $sell->save();
+                $raffle = Raffle::where('id', $key['raffle_id'])->get()->first();
+                if ($raffle == null) {
+                    return $this->errorResponse('No se encontro la rifa' . ' ' . $key['raffle_id'], 400);
+                }
             }
+
+            $price = $amount * 100;
+
+            $order_id = Str::uuid();
+            $username = "44623003";
+            $password = "2fKXe4j8dmS6n8hy";
+
+
+
+            if ($price) {
+                http_response_code(200);
+                $key = $password;  //LLAVE DEL COMERCIO
+                $vads_action_mode = "INTERACTIVE";
+                $vads_amount = $price;
+                $vads_ctx_mode = "TEST";
+                $vads_currency = "604";
+                $vads_cust_email = $user->email;
+                $vads_page_action = "ASK_REGISTER_PAY";
+                $vads_payment_config = "SINGLE";
+                $vads_site_id = $username;  // IDENTIFICADOR DEL COMERCIO
+                $vads_trans_date = gmdate("YmdGis");
+                $vads_trans_id = rand(100000, 999999);
+                $vads_version = "V2";
+                $signature = "";
+                $postfield = "";
+                $parameters_args = array(
+                    'vads_action_mode' => $vads_action_mode,
+                    'vads_amount' => $vads_amount,
+                    'vads_ctx_mode' => $vads_ctx_mode,
+                    'vads_currency' => $vads_currency,
+                    'vads_cust_email' => $vads_cust_email,
+                    'vads_page_action' => $vads_page_action,
+                    'vads_payment_config' => $vads_payment_config,
+                    'vads_site_id' => $vads_site_id,
+                    'vads_trans_date' => $vads_trans_date,
+                    'vads_trans_id' => $vads_trans_id,
+                    'vads_version' => $vads_version
+                );
+                foreach ($parameters_args as $params => $value) {
+                    $signature .= $value . '+';
+                    $postfield .= $params . "=" . $value . "&";
+                }
+                $signature = $signature . $key;
+                $signature = base64_encode(hash_hmac('sha256', $signature, $key, true));
+                $parameters_args['signature'] = $signature;
+                $postfield = $postfield . "signature=" . $signature;
+                $response = $this->curl(utf8_decode($postfield));
+                $res = json_decode($response) ;
+                /* echo $response; */
+            } else {
+                return $this->errorResponse('Algun valor en la api no se envio correctamente.', 400);
+            }
+
+
+
+
+             foreach ($car as $key) {
+
+                $sell = new UserTicket();
+                $sell->user_id = $iduser;
+                $sell->raffles_id = $key['raffle_id'];
+                $sell->quantity = $key['amount'];
+                if ($res->status == "ERROR") {
+                    $sell->status = 'failed';
+                    $sell->oreder_id = $order_id;
+                    $sell->save();
+                } else {
+                    $sell->status = 'confirmed';
+                    $sell->oreder_id = $order_id;
+                    $sell->save();
+                }
+            }
+
+        } catch (\Exception $exception) {
+            return $this->errorResponse($exception->getMessage(), 400);
         }
-    } catch (\Exception $exception) {
-        return $this->errorResponse($exception->getMessage(), 400);
-    }
 
         return $this->successResponse([
             'status' => 200,
             'message' => 'Registro de pago completo',
-            'data' =>$res
+            'data' => $res
         ]);
+    }
+
+
+    public function curl($postfield)
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, "https://secure.micuentaweb.pe/vads-payment/entry.silentInit.a");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postfield);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $server_output = curl_exec($ch);
+        curl_close($ch);
+        return $server_output;
     }
 }
